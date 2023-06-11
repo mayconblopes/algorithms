e58f51b559bd93a14510211562969bb47c73eaa8
mayconblopes <lopes.maycon@gmail.com>
2023-06-11 07:23:02 -0300

    feat(rotated-list.js)
    *Created a solution with a BinarySearch algorithm

--- rotated-list.js
+++ rotated-list.js
@@ -22,8 +22,8 @@ const tests = [
   },
   {
     name: 'A list that was rotated just once.',
-    nums: [2, 4, 6, 8, 10, 12, 14, 16],
-    output: 0,
+    nums: [16, 2, 4, 6, 8, 10, 12, 14],
+    output: 1,
   },
   {
     name: 'A list that was rotated n-1 times, where n is the size of the list.',
@@ -47,20 +47,20 @@ const tests = [
   },
 ]
 
-function determineRotations(nums) {
+function determineRotationsLinearSearch(nums) {
   /*
     Find a number whose predecessor is greater than itself
     Get the list position of this number
     If its position is greater than 0, then the list was rotated that number of times
   */
 
+  console.log('LINEAR SEARCH')
   // If the length of nums is less than or equal to 1, return 0, because it means nums has no element or just 1 element, so cannot be rotated.
   if (!(nums.length > 1)) {
     return 0
   }
 
   for (let num of nums) {
-
     // Get the position (index) of current num.
     let position = nums.indexOf(num)
 
@@ -69,21 +69,61 @@ function determineRotations(nums) {
     if (!next) {
       return 0
 
-    // Check if the current number is greater than its successor. Return the index of the successor if true, because it means that nums was rotated.
+      // Check if the current number is greater than its successor. Return the index of the successor if true, because it means that nums was rotated.
     } else if (nums[position] > nums[next]) {
       return next
     }
   }
 }
 
+function determineRotationsBinarySearch(nums) {
+  /*
+  Some visualization to help
+    [2, 4, 6, 8, 10] 0 rotations
+    [10, 2, 4, 6, 8] 1 rotations
+    [8, 10, 2, 4, 6] 2 rotations
+    [6, 8, 10, 2, 4] 3 rotations
+    [4, 6, 8, 10, 2] 4 rotations
+    [2, 4, 6, 8, 10] 5 rotations
+
+  */
+  console.log('BINARY SEARCH')
+  let lo = 0
+  let hi = nums.length - 1
+
+  let num
+  for (num in nums) {
+    let mid = Math.floor((lo + hi) / 2)
+    // middle is the answer
+    if (nums[mid + 1] && nums[mid] > nums[mid + 1]) {
+      return mid + 1
+    }
+    // answer is in at right
+    if (nums[mid] > nums[nums.length - 1]) {
+      lo = mid + 1
+    } else {
+      // answer is at left
+      hi = mid - 1
+    }
+  }
+  return 0
+}
+
+const algorithm = [
+  determineRotationsBinarySearch,
+  determineRotationsLinearSearch,
+]
 
 tests.forEach((test) => {
-  let result = determineRotations(test.nums)
+  // algorithm[0] -> BinarySearch; algorithm [1] -> LinearSearch
+  let result = algorithm[0](test.nums)
   if (result == test.output) {
     console.log(`Expected output ${test.output}, output ${result}`)
     console.log(`${test.name} -> OK`)
+    console.log('\n')
   } else {
     console.log(`Expected output ${test.output}, output ${result}`)
     console.log(`${test.name} -> FAIL`)
+    console.log('\n')
   }
 })
